555e214c0dab9f890e709c4a3759c46f3e12fdf2
Harietonyeabor <henriettaogochukwuo@gmail.com>
2022-11-28 12:46:59 -0800

    task 2

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-number = 98
-print("{:f} Battery street".format(number))#!/usr/bin/python3
-number = 98
-print("{:f} Battery street".format(number))
+print("\"Programming is like building a multilingual puzzle")
