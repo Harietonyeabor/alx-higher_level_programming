36bfe2164e6fa1ae95b0daeb256ea709276a689e
Harietonyeabor <henriettaogochukwuo@gmail.com>
2023-01-11 13:33:32 -0800

    All task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-def save_to_json_file(my_obj, filename):
-    """
-    function that writes an Object to a text file, using a JSON representation
-    """
-    import json
-    with open(filename, 'w') as f:
-        json.dump(my_obj, f)
+"""
+script to save and load
+"""
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = 'add_item.json'
+my_list = []
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
+    save_to_json_file(my_list, filename)
+
+arg_len = len(argv)
+
+if arg_len > 1:
+    for i in range(1, arg_len):
+        my_list.append(argv[i])
+
+    save_to_json_file(my_list, filename)
