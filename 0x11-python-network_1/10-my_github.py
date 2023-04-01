c71e33022b3fd44cd5f480a861e107a1540f4b73
Harietonyeabor <henriettaogochukwuo@gmail.com>
2023-04-01 12:31:16 -0700

    All task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,12 +2,12 @@
 """Take GitHub credentials (username and password) as arguments
 and uses the GitHub API to display users id"""
 
-if __name__ == '__main__':
-    import sys
-    import requests
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
-    url = "https://api.github.com/user"
-    response = requests.get(
-        url, auth=requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2]))
-    res_dict = response.json()
-    print(res_dict.get('id'))
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
